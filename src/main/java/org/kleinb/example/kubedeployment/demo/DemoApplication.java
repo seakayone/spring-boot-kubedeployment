ac49c6c1ed5cdad5dc4ac9f27859a0fba81b9c4f
Christian Kleinboelting <kleinboelting@signal7.de>
2019-08-25 21:46:18 +0200

    Use Spring Webflux

--- src/main/java/org/kleinb/example/kubedeployment/demo/DemoApplication.java
+++ src/main/java/org/kleinb/example/kubedeployment/demo/DemoApplication.java
@@ -3,17 +3,18 @@ package org.kleinb.example.kubedeployment.demo;
 import io.vavr.jackson.datatype.VavrModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
-	@Bean
-	public VavrModule vavrJacksonModule(){
-		return new VavrModule();
-	}
+  @Bean
+  public VavrModule vavrJacksonModule() {
+    return new VavrModule();
+  }
 }
--- src/main/java/org/kleinb/example/kubedeployment/demo/HomeController.java
+++ src/main/java/org/kleinb/example/kubedeployment/demo/HomeController.java
@@ -1,34 +1,39 @@
 package org.kleinb.example.kubedeployment.demo;
 
+import static java.time.Instant.now;
+
+import io.vavr.control.Try;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.time.Instant;
 import lombok.Value;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class HomeController {
 
-  @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-  public TimeAndHostName home() {
-    return new TimeAndHostName();
+  private final BuildProperties buildProperties;
+
+  public HomeController(BuildProperties buildProperties) {
+    this.buildProperties = buildProperties;
   }
-}
 
-@Value
-class TimeAndHostName {
+  @GetMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public Mono<TimeVersionAndHostName> home() {
+    return Mono.fromCallable(TimeVersionAndHostName::new);
+  }
 
-  private final Instant now = Instant.now();
-  private final String hostname = hostname();
-
-  private static String hostname() {
-    try {
-      return InetAddress.getLocalHost().getHostName();
-    } catch (UnknownHostException uhe) {
-      return "unknown";
-    }
+  @Value
+  private class TimeVersionAndHostName {
+    private final Instant now = now();
+    private final String hostname = Try
+        .of(() -> InetAddress.getLocalHost().getHostName())
+        .getOrElse("N/A");
+    private final String version = buildProperties.getVersion();
+    private final Instant buildtime = buildProperties.getTime();
   }
 }
 
