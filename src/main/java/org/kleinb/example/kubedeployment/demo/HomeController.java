b6258370258a88cd4aa13abddf70777b016399f8
Christian Kleinboelting <kleinboelting@signal7.de>
2019-08-25 19:07:16 +0200

    Return time and hostname on /

--- src/main/java/org/kleinb/example/kubedeployment/demo/HomeController.java
+++ src/main/java/org/kleinb/example/kubedeployment/demo/HomeController.java
@@ -1,5 +1,8 @@
 package org.kleinb.example.kubedeployment.demo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.Instant;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
   @GetMapping
-  public String home() {
-    return "Hello Darmstadt";
+  public TimeAndHostName home() {
+    return new TimeAndHostName();
   }
 }
+
+class TimeAndHostName {
+
+  private final Instant now = Instant.now();
+  private final String hostname = hostname();
+
+  public Instant getNow() {
+    return now;
+  }
+
+  public String getHostname() {
+    return hostname;
+  }
+
+  private static String hostname() {
+    try {
+      return InetAddress.getLocalHost().getHostName();
+    } catch (UnknownHostException uhe) {
+      return "unknown";
+    }
+  }
+}
+
